c023c090a8af50832535eea46aac558ab7817603
vagrant <mikefyke@hotmail.com>
2019-12-10 14:48:10 +0000

    Initial Commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://mike_123:<mike_123>@merntest-t1zbc.mongodb.net/test?retryWrites=true&w=majority'
+}
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+// mongoDB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log('MongoDB Connected :)'))
+    .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log("Server has started on port " + port));
+
+
