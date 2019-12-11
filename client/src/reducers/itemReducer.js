9acd50ae3edae02c96ddcd2fdf789974f98c6665
vagrant <mikefyke@hotmail.com>
2019-12-11 20:04:46 +0000

    Connecting to MongoDB with newItem

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,20 +1,18 @@
 import uuid from 'uuid';
-import {GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types';
+import {GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from '../actions/types';
 
 const initialState = {
-    items: [
-        { id: uuid(), name: 'Milk' },
-        { id: uuid(), name: 'Eggs' },
-        { id: uuid(), name: 'Radish' },
-        { id: uuid(), name: 'Nachos' }
-    ]
-}
+    items: [],
+    loading: false
+};
 
 export default function(state = initialState, action){
     switch(action.type){
         case GET_ITEMS:
             return {
-                ...state
+                ...state,
+                items: action.payload,
+                loading: false
             };
         case ADD_ITEM:
                 return{
@@ -26,6 +24,11 @@ export default function(state = initialState, action){
                 ...state,
                 items: state.items.filter(item => item.id !== action.payload)
             }
+        case ITEMS_LOADING:
+        return{
+            ...state,
+            loading: true
+        }
         default :
             return state;
     }
