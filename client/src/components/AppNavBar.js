e28385b47dca23b962b6788f5960a423e3f3bbfa
vagrant <mikefyke@hotmail.com>
2019-12-11 02:32:04 +0000

    Added dependencies for the frontend

--- client/src/components/AppNavBar.js
+++ client/src/components/AppNavBar.js
@@ -11,33 +11,36 @@ import {
 } from 'reactstrap';
 
 class AppNavBar extends Component {
-    state = {
+    constructor(props){
+    super(props);
+    this.state = {
         isOpen: false
     }
     toggle = () => {
         this.setState({
             isOpen: !this.state.isOpen
         });
-    }
+    };
+}
     // mb-5 = margin bottom 5, expand 'sm' = hamburger on smallscreens
     render(){
         return(
             <div>
-                <NavBar className="mb-5" color="dark" dark expand="sm"> 
-                <Container>
-                    <NavBarBrand href="/">Shopping List</NavBarBrand>
-                    <NavBarToggler onClick={this.toggle}></NavBarToggler>
-                    <Collapse isOpen={this.state.isOpen} navbar>
-                        <Nav className="ml-auto" navbar>
-                            <NavItem>
-                                <NavLink href="https://github.com/MikeTheFyke">
-                                    GitHub
-                                </NavLink>
-                            </NavItem>
-                        </Nav>
-                    </Collapse>
-                </Container>
-                </NavBar>
+                <Navbar className='mb-5' color='dark' dark expand='sm'> 
+                    <Container>
+                        <NavbarBrand href='/'>Shopping List</NavbarBrand>
+                        <NavbarToggler onClick={this.toggle} />
+                        <Collapse isOpen={this.state.isOpen} navbar>
+                            <Nav className='ml-auto' navbar>
+                                <NavItem>
+                                    <NavLink href="https://github.com/MikeTheFyke">
+                                        GitHub
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </Container>
+                </Navbar>
             </div>
         );
     }
