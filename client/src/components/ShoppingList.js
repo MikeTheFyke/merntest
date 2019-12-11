0392ea53103eb2c7c86c7ecc5810d64a18e003e9
vagrant <mikefyke@hotmail.com>
2019-12-11 20:17:26 +0000

    Fixed id fields in response to MongoDB _id fields

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -22,14 +22,14 @@ class ShoppingList extends Component{
             <Container>
                 <ListGroup>
                     <TransitionGroup className="shopping-list">
-                        {items.map(({ id, name }) => (
-                            <CSSTransition key={id} timeout={500} classNames="fade">
+                        {items.map(({ _id, name }) => (
+                            <CSSTransition key={_id} timeout={500} classNames="fade">
                                 <ListGroupItem className="text-success">
                                     <Button
                                         className="remove-btn text-white"
                                         color="warning"
                                         size="sm"
-                                        onClick={this.onDeleteClick.bind(this, id)}
+                                        onClick={this.onDeleteClick.bind(this, _id)}
                                     >&times;</Button>
                                     {name}
                                 </ListGroupItem>
